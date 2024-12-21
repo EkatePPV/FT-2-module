da4e6a4341f61dd0e82e4ae4c76a79dc8a02fa87
ekaterina_popova <katya2005.popova@yandex.ru>
2024-12-21 13:21:27 +0400

    Лабораторная 3

--- index.js
+++ index.js
@@ -1,47 +1,80 @@
-// №1
-const findTwoMax = (x, y, z) => {
-    const firstMax = Math.max(x, y, z);
-    const secondMax = Math.max(x === firstMax ? -Infinity : x, y === firstMax ? -Infinity : y, z === firstMax ? -Infinity : z);
-    return firstMax * secondMax;
+const studentJSON = {
+    surname: "Иванов",
+    name: "Иван",
+    marks: [
+        { subject: "Математика", mark: 5 },
+        { subject: "Физика", mark: 4 },
+        { subject: "Химия", mark: 4 }
+    ]
 };
 
-let num1 = parseInt(prompt('Задание 1\nВведите первое число'));
-let num2 = parseInt(prompt('Введите второе число'));
-let num3 = parseInt(prompt('Введите третье число'));
-alert(`Произведение двух наибольших чисел: ` + findTwoMax(num1, num2, num3));
-
-// №2
-const greetUser = (userName, userRank = "рядовой") => `Здравствуйте, ${userRank} ${userName}`;
-
-alert(greetUser("Алексей"));
-alert(greetUser("Дмитрий", "сержант")); 
-
-// №3
-const executeFunction = (fn, ...params) => fn(...params);
-
-const abFunction = (a, b) => 2*a + (3*b)/a;
-
-alert(`Результат функции 2*a+(3*b)/a при a=2, b=1 : ` + executeFunction(abFunction, 2, 1));
-
-// №4
-const powerFuncCreator = (value) => value % 2 === 0 ? (num) => num ** 2 : (num) => num ** 3;
-
-const userInput = prompt("Задание 4\nВведите целое число:");
-const parsedNumber = parseInt(userInput, 10);
-if (!isNaN(parsedNumber)) {
-    const powerFunc = powerFuncCreator(parsedNumber);
-    const output = powerFunc(parsedNumber); 
-    alert(`Результат: ${output}`);
-} else {
-    alert(`Пожалуйста, введите корректное целое число`);
+function printStudentInfo(student) {
+    console.log(`Фамилия: ${student.surname}`);
+    console.log(`Имя: ${student.name}`);
+    console.log("Оценки:");
+    student.marks.forEach(mark => {
+        console.log(` - ${mark.subject}: ${mark.mark}`);
+    });
 }
 
-// №5
-const calculateFactorial = (n) => {
-    if (n < 0) return undefined; 
-    if (n === 0) return 1;
-    return n * calculateFactorial(n - 1);
-};
+printStudentInfo(studentJSON);
 
-let factorialInput = parseInt(prompt('Задание 5\nВведите число для вычисления факториала'));
-alert(`Факториал числа ${factorialInput}: ${calculateFactorial(factorialInput)}`);
+class Mark {
+    constructor(subject, mark) {
+        this.subject = subject;
+        this.mark = mark;
+    }
+}
+
+class Student {
+    constructor(surname, name) {
+        this.surname = surname;
+        this.name = name;
+        this.marks = [];
+    }
+
+    addMark(subject, mark) {
+        const newMark = new Mark(subject, mark);
+        this.marks.push(newMark);
+    }
+
+    removeMarksBySubject(subject) {
+        this.marks = this.marks.filter(mark => mark.subject !== subject);
+    }
+
+    getAverageMark() {
+        if (this.marks.length === 0) return 0;
+        const total = this.marks.reduce((sum, mark) => sum + mark.mark, 0);
+        return total / this.marks.length;
+    }
+
+    getMarksBySubject(subject) {
+        return this.marks.filter(mark => mark.subject === subject);
+    }
+
+    printInfo() {
+        console.log(`Фамилия: ${this.surname}`);
+        console.log(`Имя: ${this.name}`);
+        console.log("Оценки:");
+        this.marks.forEach(mark => {
+            console.log(` - ${mark.subject}: ${mark.mark}`);
+        });
+    }
+}
+
+const student = new Student("Павлов", "Георгий");
+student.addMark("Математика", 5);
+student.addMark("Математика", 4);
+student.addMark("Математика", 5);
+student.addMark("Физика", 4);
+student.addMark("Физика", 5);
+student.addMark("Химия", 3);
+
+student.printInfo();
+console.log(`Средняя оценка: ${student.getAverageMark()}`);
+
+console.log("Оценки по физике:", student.getMarksBySubject("Физика"));
+
+student.removeMarksBySubject("Химия");
+console.log("Удалены оценки по химии:");
+student.printInfo();
