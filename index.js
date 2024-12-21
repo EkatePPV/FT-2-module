238793189935b7b0bfdd870b1fc6569f8d655a58
ekaterina_popova <katya2005.popova@yandex.ru>
2024-12-21 12:45:21 +0400

    Лабораторная 2

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
-/* Напишите исходный код программы, которая считывает единственное
-целое число c и проверяет, является ли оно негипотенузным.
-Негипотенузное число – натуральное число, квадрат которого не
-может быть записан как сумма двух ненулевых квадратов чисел.
-Примеры негипотенузных чисел: 1, 2, 3, 4, 6, 7, ....*/
+// №1
+const findTwoMax = (x, y, z) => {
+    const firstMax = Math.max(x, y, z);
+    const secondMax = Math.max(x === firstMax ? -Infinity : x, y === firstMax ? -Infinity : y, z === firstMax ? -Infinity : z);
+    return firstMax * secondMax;
+};
 
-const input = prompt("Введите целое число c:");
-const number = parseInt(input);
+let num1 = parseInt(prompt('Задание 1\nВведите первое число'));
+let num2 = parseInt(prompt('Введите второе число'));
+let num3 = parseInt(prompt('Введите третье число'));
+alert(`Произведение двух наибольших чисел: ` + findTwoMax(num1, num2, num3));
 
-if (!isNaN(number)) {
-    if (number <= 0) {
-        alert("Пожалуйста, введите корректное целое число.");
-    } else {
-        const square = number * number;
-        let isNonHypotenuse = true;
+// №2
+const greetUser = (userName, userRank = "рядовой") => `Здравствуйте, ${userRank} ${userName}`;
 
-        for (let a = 1; a * a < square; a++) {
-            const bSquare = square - a * a;
-            const b = Math.sqrt(bSquare);
+alert(greetUser("Алексей"));
+alert(greetUser("Дмитрий", "сержант")); 
 
-            if (b === Math.floor(b) && b > 0) {
-                isNonHypotenuse = false;
-                break;
-            }
-        }
+// №3
+const executeFunction = (fn, ...params) => fn(...params);
 
-        if (isNonHypotenuse) {
-            alert(`${number} является негипотенузным числом`);
-        } else {
-            alert(`${number} является гипотенузным числом`);
-        }
-    }
+const abFunction = (a, b) => 2*a + (3*b)/a;
+
+alert(`Результат функции 2*a+(3*b)/a при a=2, b=1 : ` + executeFunction(abFunction, 2, 1));
+
+// №4
+const powerFuncCreator = (value) => value % 2 === 0 ? (num) => num ** 2 : (num) => num ** 3;
+
+const userInput = prompt("Задание 4\nВведите целое число:");
+const parsedNumber = parseInt(userInput, 10);
+if (!isNaN(parsedNumber)) {
+    const powerFunc = powerFuncCreator(parsedNumber);
+    const output = powerFunc(parsedNumber); 
+    alert(`Результат: ${output}`);
 } else {
     alert(`Пожалуйста, введите корректное целое число`);
 }
+
+// №5
+const calculateFactorial = (n) => {
+    if (n < 0) return undefined; 
+    if (n === 0) return 1;
+    return n * calculateFactorial(n - 1);
+};
+
+let factorialInput = parseInt(prompt('Задание 5\nВведите число для вычисления факториала'));
+alert(`Факториал числа ${factorialInput}: ${calculateFactorial(factorialInput)}`);
